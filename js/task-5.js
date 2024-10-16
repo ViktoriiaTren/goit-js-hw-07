13e8bce42c2a69daf65177d2cf06262665ba2c42
ViktoriiaTren <viktoria.tren@gmail.com>
2024-10-16 17:59:53 +0300

    update

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+});
